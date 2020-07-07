64580889e6b5d4446a13ff52f0ce4b13aa812495
calbertuk <charlie.allombert@gmail.com>
2020-07-07 11:31:25 +0100

    add dependecies

---
+++ run.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '.commands') {
+    client.channels.cache.get(msg.channel.id).send(`The list of help commands are: `);
+  }
+  if (msg.content === '.crv') {
+    client.channels.cache.get(msg.channel.id).send(`The Curve native token (CRV) has not been released yet. If you wish to find more information about click the following link: https://guides.curve.fi/everything-you-need-to-know-about-crv/`);
+  }
+
+});
+
+
+client.login("NzI5ODE0NDY3NDk5NTI0MDk2.XwOa3g.Ov5Sr498d6Ch42YnmaqHoYFVCg0");
