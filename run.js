f39c74fd82125a3bec0d2c2afab609bd94ed6ea2
calbertuk <charlie.allombert@gmail.com>
2020-08-22 14:37:08 +0100

    removed spaces from market command

--- run.js
+++ run.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const axios = require("axios");
+const retry = require('async-retry')
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
-let commandList = ['.announcements', '.audit', '.commands', '.contracts', '.crv', '.farming', '.guides', '.risks', '.rules', '.twitter', '.wen', '.when', '.whisper', '.market'];
+let commandList = ['.announcements', '.audit', '.commands', '.contracts', '.crv', '.farming', '.guides','.market', '.now', '.risks', '.rules', '.twitter', '.when', '.wen', '.whisper', ];
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.content === '.commands') {
     client.channels.cache.get(msg.channel.id).send(`The list of help commands is: ${makeList()}`);
   }
@@ -20,10 +23,10 @@ client.on('message', msg => {
   if (msg.content === '.audit') {
     client.channels.cache.get(msg.channel.id).send(`Find details about Curve Finance audits at this address: https://www.curve.fi/audits`);
   }
-  if (msg.content === '.contracts') {
+  if (msg.content === '.contracts' || msg.content == '!contracts' || msg.content == '!Contracts' || msg.content == '!contract') {
     client.channels.cache.get(msg.channel.id).send(`You can read our smart contracts at this address: https://www.curve.fi/contracts`);
   }
-  if (msg.content === '.crv' || msg.content === '.when' || msg.content === '.wen') {
+  if (msg.content === '.crv' || msg.content === '.when' || msg.content === '.wen' || msg.content === '.now') {
     client.channels.cache.get(msg.channel.id).send(`Now: https://guides.curve.fi/crv-launches-curve-dao-and-crv/`);
   }
   if (msg.content === '.farming') {
@@ -45,27 +48,18 @@ client.on('message', msg => {
     client.channels.cache.get(msg.channel.id).send(`https://i.imgur.com/skgQJ3K.jpg`);
   }
   if (msg.content === '.market') {
-    axios({
-      method: 'get',
-      url: 'https://api.coingecko.com/api/v3/simple/price?ids=curve-dao-token&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true',
-      responseType: 'json'
-    })
-      .then(function(response) {
-      if(client.channels.cache.get(msg.channel.id) == 735144456914731098){
-      client.channels.cache.get(msg.channel.id).send(`Price: $${response.data['curve-dao-token'].usd}
-24H Volume: $${numberWithCommas(response.data['curve-dao-token'].usd_24h_vol.toFixed(2))}
-24H Change: ${response.data['curve-dao-token'].usd_24h_change.toFixed(2)} %
-Market Cap: $${numberWithCommas(response.data['curve-dao-token'].usd_market_cap.toFixed(2))}`);
-}
-    });
+        const options = {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+        };
+        let response = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=curve-dao-token&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))
+        if(client.channels.cache.get(msg.channel.id) == 735144456914731098){
+            client.channels.cache.get(msg.channel.id).send(`Price: ${response.data['curve-dao-token'].usd}$\n\r24H Volume: ${Number(response.data['curve-dao-token'].usd_24h_vol).toLocaleString('en', options)}$\n\r24H Change: ${response.data['curve-dao-token'].usd_24h_change.toFixed(2)} %`);
+        }
   }
 
 
 });
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
 
 
 var makeList = () => {
